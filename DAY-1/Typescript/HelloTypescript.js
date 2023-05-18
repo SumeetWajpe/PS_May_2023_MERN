4d00b54467805f5917838d7107d26fc52b1e676d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-18 10:05:28 +0530

    📝 Function Parameter types

--- DAY-1/Typescript/HelloTypescript.js
+++ DAY-1/Typescript/HelloTypescript.js
@@ -1,5 +1,22 @@
 //var x = 100; // Type inferencing
 // x = "Hello !"; // Error
-var x; // Type annotation
+// var x: number; // Type annotation
+// x = 100;
+// console.log(typeof x);
+// var str: string;
+// var bool: boolean;
+// var z; // implicitly of type any (bad practise)
+// z = 10;
+// z = true;
+// z = "Hi";
+// z = {};
+// z = [];
+var x;
+x = "Hi";
 x = 100;
-console.log(typeof x);
+// Functions
+function Add(x, y) {
+    return x + y;
+}
+Add(20, 30);
+Add("Hello", "World !");
--- DAY-1/Typescript/HelloTypescript.ts
+++ DAY-1/Typescript/HelloTypescript.ts
@@ -1,15 +1,28 @@
 //var x = 100; // Type inferencing
 // x = "Hello !"; // Error
 
-var x: number; // Type annotation
-x = 100;
-console.log(typeof x);
-var str: string;
-var bool: boolean;
+// var x: number; // Type annotation
+// x = 100;
+// console.log(typeof x);
+// var str: string;
+// var bool: boolean;
 
-var z;
-z = 10;
-z = true;
-z = "Hi";
-z = {};
-z = [];
+// var z; // implicitly of type any (bad practise)
+// z = 10;
+// z = true;
+// z = "Hi";
+// z = {};
+// z = [];
+
+var x: number | string;
+
+x = "Hi";
+x = 100;
+
+// Functions
+function Add(x: number, y: number) {
+  return x + y;
+}
+
+Add(20, 30);
+// Add("Hello", "World !"); // Error !
