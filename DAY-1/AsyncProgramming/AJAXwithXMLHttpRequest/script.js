971e372a42fab47f06599f278108e97666a08634
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-18 16:25:45 +0530

    📝 XMLHttpRequest - Basics

---
+++ DAY-1/AsyncProgramming/AJAXwithXMLHttpRequest/script.js
@@ -0,0 +1,16 @@
+function GetData() {
+  //1. XMLHttpRequest object
+  //2. open()
+  //3. register for onreadystatechange event
+  //4. making ajax call with send()
+  //5. Get the reponse & use it
+
+  let xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xmlHttpReq.onreadystatechange = function () {
+    if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
+      console.log(xmlHttpReq.responseText);
+    }
+  };
+  xmlHttpReq.send();
+}
