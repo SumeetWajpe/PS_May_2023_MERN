f43ef1ff2e297ee581d79f1b3ab454755ed2544e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-19 13:28:27 +0530

    📝 Using String Literals

---
+++ DAY-2/Typescript/StringLiterals/app.js
@@ -0,0 +1,7 @@
+var person = { name: "Federer", age: 40 };
+console.log("The person " + person.name + " is  " + person.age + " years old !");
+console.log(`The person ${person.name} is ${person.age} years old !`);
+let multiLineStr = `This is first Line !
+Second Line
+Last Liine !`;
+console.log(multiLineStr);
