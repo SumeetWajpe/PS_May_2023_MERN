9b3bf51a4b19abd351268b4811dbf5daa5048530
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-19 13:16:02 +0530

    📝 Using tsconfig.json

---
+++ DAY-2/Typescript/Usingtsconfig/app.ts
@@ -0,0 +1,7 @@
+const PI = 3.14;
+let x = 100;
+// x = "Hello";
+
+function Square(x: number) {
+  return x * x;
+}
---
+++ DAY-2/Typescript/Usingtsconfig/dist/app.js
@@ -0,0 +1,6 @@
+var PI = 3.14;
+var x = 100;
+// x = "Hello";
+function Square(x) {
+    return x * x;
+}
