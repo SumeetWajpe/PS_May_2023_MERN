48e9e331d5dc5e8b6a95d3a4148c33e0f75a5531
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-19 11:04:08 +0530

    📝 Handling errors with try/catch - async await

--- DAY-1/AsyncProgramming/UsingAsyncAwait/GetData.js
+++ DAY-1/AsyncProgramming/UsingAsyncAwait/GetData.js
@@ -1,7 +1,7 @@
 function GetData() {
   return new Promise((resolve, reject) => {
     let xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/postsss");
     xmlHttpReq.onreadystatechange = function () {
       if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
         resolve(xmlHttpReq.responseText);
