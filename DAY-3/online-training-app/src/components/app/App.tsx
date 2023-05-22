9db694465d72a86c41706ac50526bb5159e5cb5c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-22 16:54:15 +0530

    ✨ using useEffect()

--- DAY-3/online-training-app/src/components/app/App.tsx
+++ DAY-3/online-training-app/src/components/app/App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ListOfCourses from "../listofcourses/listofcourses.component";
 import { Message } from "../functional/message.functional";
 import { Counter } from "../functional/counter";
+import Posts from "../posts/posts.component";
 
 export default function App() {
-  return <ListOfCourses />;
+  // return <ListOfCourses />;
+  return <Posts />;
 }
---
+++ DAY-3/online-training-app/src/components/posts/posts.component.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+type PostsModel = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+const Posts = () => {
+  let [posts, setPosts] = useState<PostsModel[]>([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(postsResponse => setPosts(postsResponse));
+  });
+  return (
+    <div>
+      <header>
+        <h1>All Posts</h1>
+      </header>
+      <main>
+        <ul>
+          {posts.map(post => (
+            <li>{post.title}</li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default Posts;
