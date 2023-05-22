499a327ce3261db8a2da84fdb693e255c056cfec
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-22 12:08:07 +0530

    📝 Iterating through collectio (array) to generate dynamic instances

--- DAY-3/my-first-app/src/App.tsx
+++ DAY-3/my-first-app/src/App.tsx
@@ -5,30 +5,32 @@ import { MessageModel } from "./models/message.model";
 import { Message } from "./components/message/message.component";
 
 class App extends React.Component {
-  messageOne: MessageModel = new MessageModel(
-    "Hello",
-    "John",
-    "Carter",
-    "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Hello_Web_Series_%28Wordmark%29_Logo.png/1200px-Hello_Web_Series_%28Wordmark%29_Logo.png",
-  );
-  messageTwo: MessageModel = new MessageModel(
-    "Hey",
-    "John",
-    "Alen",
-    "https://www.shutterstock.com/image-vector/hey-speech-bubble-retro-style-260nw-556644574.jpg",
-  );
-  messageThree: MessageModel = new MessageModel(
-    "Hola",
-    "Mark",
-    "Alen",
-    "https://www.shutterstock.com/image-vector/hola-word-lettering-spanish-hello-260nw-2170222797.jpg",
-  );
+  messages: MessageModel[] = [
+    new MessageModel(
+      "Hello",
+      "John",
+      "Carter",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Hello_Web_Series_%28Wordmark%29_Logo.png/1200px-Hello_Web_Series_%28Wordmark%29_Logo.png",
+    ),
+    new MessageModel(
+      "Hey",
+      "John",
+      "Alen",
+      "https://www.shutterstock.com/image-vector/hey-speech-bubble-retro-style-260nw-556644574.jpg",
+    ),
+    new MessageModel(
+      "Hola",
+      "Mark",
+      "Alen",
+      "https://www.shutterstock.com/image-vector/hola-word-lettering-spanish-hello-260nw-2170222797.jpg",
+    ),
+  ];
   render() {
     return (
       <div className="App">
-        <Message msg={this.messageOne} />
-        <Message msg={this.messageTwo} />
-        <Message msg={this.messageThree} />
+        {this.messages.map((message: MessageModel) => (
+          <Message msg={message} />
+        ))}
       </div>
     );
   }
