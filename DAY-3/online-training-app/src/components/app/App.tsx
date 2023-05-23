08abeb54b8cf1cd7372db9017f2e4d8adbbc7fca
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-23 16:29:04 +0530

    ✨ Routing Basics

--- DAY-3/online-training-app/src/components/app/App.tsx
+++ DAY-3/online-training-app/src/components/app/App.tsx
@@ -5,8 +5,18 @@ import { Counter } from "../functional/counter";
 import Posts from "../posts/posts.component";
 import GetpostById from "../postbyid/getpostbyid";
 import { GrandParent } from "../usingcontextapi/usingcontextapi.component";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <ListOfCourses />;
-  // return <GrandParent />;
+  return (
+    <>
+      <BrowserRouter>
+        <Link to="/">Courses</Link> | <Link to="/posts">Posts </Link>
+        <Routes>
+          <Route path="/" element={<ListOfCourses />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
