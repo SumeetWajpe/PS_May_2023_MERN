72f7db813be985a156107f83ea3db7272c26e8a0
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-24 14:12:46 +0530

    📝 NewCourse Form

--- DAY-3/online-training-app/src/components/newcourse/newcourse.component.tsx
+++ DAY-3/online-training-app/src/components/newcourse/newcourse.component.tsx
@@ -4,6 +4,54 @@ export default function NewCourse() {
   return (
     <div>
       <h2>New Course</h2>
+
+      <form>
+        <label>
+          Id : <input type="number" className="form-control" />
+        </label>
+        <br />
+        <label>
+          Title : <input type="text" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Price : <input type="number" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Rating : <input type="number" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Like : <input type="number" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Trainer Name : <input type="text" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Avatar Url : <input type="text" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Image Url : <input type="text" className="form-control" />
+        </label>
+        <br />
+
+        <label>
+          Description : <input type="text" className="form-control" />
+        </label>
+        <br />
+
+        <button className="btn btn-success m-1">Add New Course</button>
+      </form>
     </div>
   );
 }
