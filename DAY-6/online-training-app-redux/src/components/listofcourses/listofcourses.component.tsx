3871f5ea5084b1ebe57a9d44d17fe65fc3d41fef
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-26 17:04:13 +0530

    ✨ Using Thunk

--- DAY-6/online-training-app-redux/src/components/listofcourses/listofcourses.component.tsx
+++ DAY-6/online-training-app-redux/src/components/listofcourses/listofcourses.component.tsx
@@ -1,13 +1,21 @@
 import Course from "../course/course.component";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store/store";
+import { useEffect } from "react";
+import { fetchCoursesAsync } from "../../redux/reducers/courses.reducer";
+import { AppDispatch } from "../../redux/store/store";
 
 export default function ListOfCourses() {
   const courses = useSelector((state: RootState) => state.courses);
-
+  const dispatch = useDispatch<AppDispatch>();
   let coursesToBeCreated = courses.map(course => (
     <Course coursedetails={course} key={course.id} />
   ));
+
+  useEffect(() => {
+    dispatch(fetchCoursesAsync());
+  }, []);
+
   return (
     <>
       <header>
