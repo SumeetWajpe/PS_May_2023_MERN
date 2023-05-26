7556dc17b381d7a5b3b7b04423d0f67975b4acbc
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-26 09:50:54 +0530

    📝 Using useDispatch Hook

--- DAY-6/online-training-app-redux/src/App.tsx
+++ DAY-6/online-training-app-redux/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { incrementLikes } from "./redux/reducers/courses.reducer";
 
 function App() {
   const courses = useSelector((store: any) => store.courses);
-  return <div className="App">{courses.length}</div>;
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      {courses.length}
+
+      <button onClick={() => dispatch(incrementLikes("100"))}>++</button>
+    </div>
+  );
 }
 
 export default App;
