12eb43cf6102fe7ffa833390a29be30296bf5bef
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-29 12:45:34 +0530

    📝 Configuring store with Saga

--- DAY-6/online-training-app-redux/src/redux/store/store.ts
+++ DAY-6/online-training-app-redux/src/redux/store/store.ts
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import coursesReducer from "../reducers/courses.reducer";
 import postsReducer from "../reducers/posts.reducer";
 import cartReducer from "../reducers/cart.reducer";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../../saga/sagas";
 
 const store = configureStore({
   reducer: {
@@ -9,7 +11,13 @@ const store = configureStore({
     posts: postsReducer,
     cart: cartReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat([sagaMiddleware]),
 });
+
+const sagaMiddleware = createSagaMiddleware();
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
