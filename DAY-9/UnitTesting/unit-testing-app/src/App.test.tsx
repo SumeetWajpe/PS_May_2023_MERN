338a7b3aad78d4d9636b63c78975b468a15340bc
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-30 14:30:56 +0530

    ✨ Using Enzyme

--- DAY-9/UnitTesting/unit-testing-app/src/App.test.tsx
+++ DAY-9/UnitTesting/unit-testing-app/src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App, { CounterState } from "./App";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("test suite for counter", () => {
+  it("tests if counter is zero", () => {
+    let appInstance = shallow(<App />);
+    let state = appInstance.state() as CounterState;
+    expect(state.count).toBe(10);
+  });
 });
--- DAY-9/UnitTesting/unit-testing-app/src/App.tsx
+++ DAY-9/UnitTesting/unit-testing-app/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export type CounterState = {
+  count: number;
+};
+
+export default class App extends React.Component<{},CounterState> {
+  state: Readonly<CounterState> = { count: 0 };
+  render(): React.ReactNode {
+    return (
+      <>
+        <strong>{this.state.count}</strong>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          ++
+        </button>
+      </>
+    );
+  }
 }
-
-export default App;
