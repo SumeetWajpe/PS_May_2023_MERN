27ea81b961ce49be0284f3f6fef43f43d6388c1e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-30 16:42:19 +0530

    📝 Using fs - blocking way

--- DAY-9/Node/FileSystem/app.js
+++ DAY-9/Node/FileSystem/app.js
@@ -1,12 +1,25 @@
+// Non -blocking approach
+// const fs = require("fs");
+
+// // node -> callback(err,data)
+// fs.readFile("Input.txt", (err, dataFromFile) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(dataFromFile.toString());
+//   }
+// });
+
+// console.log("Program Ended !");
+
+// Blocking approach
+
 const fs = require("fs");
 
-// node -> callback(err,data)
-fs.readFile("Input.txt", (err, dataFromFile) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(dataFromFile.toString());
-  }
-});
+console.log("Program Started..");
+
+let content = fs.readFileSync("Input.txt");
+
+console.log(content.toString());
 
 console.log("Program Ended !");
