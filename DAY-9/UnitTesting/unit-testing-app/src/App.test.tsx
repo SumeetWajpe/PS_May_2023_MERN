81bbadeab2381d15727074d1279036a078d50439
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-30 14:37:38 +0530

    📝 Testing Button Click event & DOM

--- DAY-9/UnitTesting/unit-testing-app/src/App.test.tsx
+++ DAY-9/UnitTesting/unit-testing-app/src/App.test.tsx
@@ -9,6 +9,14 @@ describe("test suite for counter", () => {
   it("tests if counter is zero", () => {
     let appInstance = shallow(<App />);
     let state = appInstance.state() as CounterState;
-    expect(state.count).toBe(10);
+    expect(state.count).toBe(0);
+  });
+
+  it("increments counter on click of the button", () => {
+    let appInstance = shallow(<App />);
+    let btn = appInstance.find("button");
+    btn.simulate("click");
+    let countText = appInstance.find("strong").text();
+    expect(countText).toBe("1")
   });
 });
