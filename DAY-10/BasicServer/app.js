4ebddcc87545a738a9d211a51520088589710536
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-31 14:11:26 +0530

    📝 Updated basic server to serve static files

--- DAY-10/BasicServer/app.js
+++ DAY-10/BasicServer/app.js
@@ -4,16 +4,29 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  fs.readFile("Index.html", (err, dataFromFile) => {
-    if (err) {
-      console.log("Error !");
-      console.log(err);
-    } else {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(dataFromFile);
-    }
-  });
+  if (req.url == "/") {
+    fs.readFile("Index.html", (err, dataFromFile) => {
+      if (err) {
+        console.log("Error !");
+        console.log(err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(dataFromFile);
+      }
+    });
+  } else if (req.url == "/script.js") {
+    fs.readFile("script.js", (err, dataFromFile) => {
+      if (err) {
+        console.log("Error !");
+        console.log(err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/javascript");
+        res.end(dataFromFile);
+      }
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
