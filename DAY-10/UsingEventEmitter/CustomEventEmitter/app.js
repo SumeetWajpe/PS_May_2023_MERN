d5d3bef1045d5d947cde180130ac85137968a42a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-31 10:43:58 +0530

    📝 Publishing & Subscribing to custom events

--- DAY-10/UsingEventEmitter/CustomEventEmitter/app.js
+++ DAY-10/UsingEventEmitter/CustomEventEmitter/app.js
@@ -6,17 +6,28 @@ function GetCount(maxIteration) {
   let e = new EventEmitter();
   // logic
 
-  setTimeout(() => {
+  process.nextTick(() => {
     e.emit("start");
-  }, 2000);
+    let counter = 0;
+    let timer = setInterval(() => {
+      if (counter < maxIteration) {
+        e.emit("data", counter++);
+      } else if (counter == maxIteration) {
+        clearInterval(timer);
+      }
+    }, 500);
+  });
   return e;
 }
 
 //// =======================
 // Subscriber
-let evt = GetCount();
+let evt = GetCount(10);
 evt.on("start", () => {
   console.log("Started..");
+});
+evt.on("data", currCount => {
+  console.log("Received Data -> " + currCount);
 });
 
 console.log("Program Ended !");
