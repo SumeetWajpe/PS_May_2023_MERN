17951e8ce1dcdc23e1713f8ccc8c427b5d8c8c42
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-31 11:37:10 +0530

    ✨ A basic Node Server

--- DAY-10/Streams/ReadableAndWritableStreams/app.js
+++ DAY-10/Streams/ReadableAndWritableStreams/app.js
@@ -3,15 +3,18 @@ const fs = require("fs");
 let readableStream = fs.createReadStream("Input.txt", "utf-8"); // provided the encoding
 let writableStream = fs.createWriteStream("Output.txt", "utf-8");
 
-let dataToBewritten = "";
-readableStream.on("data", chunk => {
-  // console.log("\r\n>>>>>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>>>>>>>>>>>>>> \r\n");
-  dataToBewritten += chunk;
-});
+// let dataToBewritten = "";
+// readableStream.on("data", chunk => {
+//   // console.log("\r\n>>>>>>>>>>>>>>>>>>>>> CHUNK >>>>>>>>>>>>>>>>>>>>>>>> \r\n");
+//   dataToBewritten += chunk;
+// });
 
-readableStream.on("end", () => {
-  writableStream.write(dataToBewritten);
-  writableStream.end();
-});
+// readableStream.on("end", () => {
+//   writableStream.write(dataToBewritten);
+//   writableStream.end();
+// });
 
+// OR
+
+readableStream.pipe(writableStream);
 console.log("Program Ended");
