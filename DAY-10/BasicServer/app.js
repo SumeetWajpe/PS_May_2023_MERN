ad4be954a903eecb7ed004ebe2c61d1c2c8c422c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-31 11:44:22 +0530

    📝 Returning HTML pages - using fs

--- DAY-10/BasicServer/app.js
+++ DAY-10/BasicServer/app.js
@@ -1,12 +1,18 @@
 const http = require("http"); // built-in
-
+const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  fs.readFile("Index.html", (err, dataFromFile) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(dataFromFile);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
