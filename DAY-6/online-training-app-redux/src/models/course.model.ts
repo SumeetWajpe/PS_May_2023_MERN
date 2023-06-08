93524470eb5a7b57592599a6ef597f49f2fb9491
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-08 11:25:12 +0530

    ✨ Pubdemy Project

--- DAY-6/online-training-app-redux/src/models/course.model.ts
+++ DAY-6/online-training-app-redux/src/models/course.model.ts
@@ -1,13 +1,25 @@
-export class CourseModel {
-  constructor(
-    public id: number = 0,
-    public title: string = "",
-    public price: number = 0,
-    public rating: number = 0,
-    public likes: number = 0,
-    public imageUrl: string = "",
-    public trainerName: string = "",
-    public avatarUrl: string = "",
-    public description: string = "",
-  ) {}
+export interface ICourseModel {
+  id: number;
+  title: string;
+  price: number;
+  rating: number;
+  likes: number;
+  imageUrl: string;
+  trainerName: string;
+  avatarUrl: string;
+  description: string;
 }
+export class CourseModel implements ICourseModel {
+  public id: number = 0;
+  public title: string = "";
+  public price: number = 0;
+  public rating: number = 0;
+  public likes: number = 0;
+  public imageUrl: string = "";
+  public trainerName: string = "";
+  public avatarUrl: string = "";
+  public description: string = "";
+  public xyz: string = "123";
+}
+
+var cm = new CourseModel();
